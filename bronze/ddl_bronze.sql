6cef7a0f797336d8243ef80eda76e2d545c32944
TedLuckins <tedluckins@gmail.com>
2025-11-21 14:01:32 +0000

    Rename and modify crm_custom_info table

--- bronze/ddl_bronze.sql
+++ bronze/ddl_bronze.sql
@@ -8,14 +8,14 @@ Script purpose:
 =================================================================================
 */
 
-IF OBJECT_ID ('bronze.crm_custom_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_custom_info;
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
 	cst_id INT,
-	cstlwy NVARCHAR(50),
+	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
 	cst_lastname NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
 	cst_gndr NVARCHAR(50),
 	cst_create_date DATE
 );
